1536ff13ad2c312c1c10cdd8bb196c609680064c
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-05-09 00:21:51 +1000

    fix anchor rel security risk

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -2,5 +2,5 @@ import './footer.scss'
 
 export const Footer = () => <footer>
     <span>2022 </span>
-    <span><a href="https://github.com/yzAlvin/" target="_blank">Alvin Zhao</a></span>
+    <span><a href="https://github.com/yzAlvin/" target="_blank" rel="noreferrer">Alvin Zhao</a></span>
 </footer>
