91c49e32e48e2054d330e16ed7d203bd333c391b
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-05-17 10:50:56 +1000

    add fast food

--- src/Cards/FastFoodRestaurants.ts
+++ src/Cards/FastFoodRestaurants.ts
@@ -2,4 +2,13 @@ export const fastFoods: string[] = [
     "Red Rooster",
     "Taco Bell",
     "KFC",
+    "Boost Juice",
+    "Donut King",
+    "Hungry Jacks",
+    "Noodle Box",
+    "Oporto",
+    "Pancake Parlour",
+    "Sumo Salad",
+    "Zambrero",
+    "McDonald's"
 ];
